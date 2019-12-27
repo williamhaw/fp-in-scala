e2f709d5c1b68a6debea8a2645d81360763b10f0
William Haw <william.haw@agoda.com>
2019-12-27 17:20:20 +0800

    Exercise 4.1

---
+++ src/main/scala/chapter4/chapter4.scala
@@ -0,0 +1,24 @@
+package chapter4
+
+package object chapter4 {
+
+  sealed trait Option[+A]{
+    def map[B](f: A => B): Option[B] = this match {
+      case None => None
+      case Some(v) => Some(f(v))
+    }
+    def flatMap[B](f: A => Option[B]): Option[B] = map(f) getOrElse None
+    def getOrElse[B >: A](default: => B): B = this match {
+      case None => default
+      case Some(v) => v
+    }
+    def orElse[B >: A](ob: => Option[B]): Option[B] = this map(Some(_)) getOrElse(ob)
+    def filter(f: A => Boolean): Option[A] = this match {
+      case Some(v) if f(v) => this
+      case _ => None
+    }
+  }
+  case class Some[+A](get: A) extends Option[A]
+  case object None extends Option[Nothing]
+
+}
---
+++ src/test/scala/chapter4/Chapter4Spec.scala
@@ -0,0 +1,41 @@
+package chapter4
+
+import org.scalatest.{FunSuite, Matchers}
+import chapter4._
+
+class Chapter4Spec extends FunSuite with Matchers {
+  test("map") {
+    Some("hey").map(_ + " world") shouldEqual Some("hey world")
+    val none: Option[Int] = None
+    none.map(_ + 1) shouldEqual None
+  }
+
+  test("flatMap") {
+    def lookup(v: String): Option[String] = v match {
+      case "hey" => Some("world")
+      case _ => None
+    }
+
+    Some("hey").flatMap(lookup) shouldEqual Some("world")
+    Some("not hey").flatMap(lookup) shouldEqual None
+    None.flatMap(lookup) shouldEqual None
+  }
+
+  test("getOrElse") {
+    Some("hey").getOrElse("world") shouldEqual "hey"
+    None.getOrElse("world") shouldEqual "world"
+  }
+
+  test("orElse") {
+    Some("hey").orElse(Some("world")) shouldEqual Some("hey")
+    Some("hey").orElse(None) shouldEqual Some("hey")
+    None.orElse(Some("world")) shouldEqual Some("world")
+    None.orElse(None) shouldEqual None
+  }
+
+  test("filter") {
+    Some("hey").filter(_ == "hey") shouldEqual Some("hey")
+    Some("not hey").filter(_ == "hey") shouldEqual None
+    None.filter(_ == "hey") shouldEqual None
+  }
+}
