8931730152bcbb290b8cc9d1ff81ac0010d280b3
William Haw <william.haw@agoda.com>
2019-12-30 17:32:17 +0800

    Fix type signature of startsWith

--- src/main/scala/chapter5/chapter5.scala
+++ src/main/scala/chapter5/chapter5.scala
@@ -89,7 +89,7 @@ package object chapter5 {
       all(s2)((_, _))
     }
 
-    def startsWith[A](s: Stream[A]): Boolean = zipAll(s).takeWhile(_._2.isDefined).forall { case (h1, h2) => h1 == h2 }
+    def startsWith[B](s: Stream[B]): Boolean = zipAll(s).takeWhile(_._2.isDefined).forall { case (h1, h2) => h1 == h2 }
   }
 
   case object Empty extends Stream[Nothing]
