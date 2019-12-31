4ea9a78f594f8395cdac49aebe995f5ba1a2cd22
William Haw <william.haw@agoda.com>
2019-12-31 19:37:29 +0800

    Add exercise comment markers for Chapter 2

--- src/main/scala/chapter2/chapter2.scala
+++ src/main/scala/chapter2/chapter2.scala
@@ -1,29 +1,36 @@
 import scala.annotation.tailrec
 
- package object chapter2 {
+package object chapter2 {
+  //Exercise 2.1
   //get nth Fibonacci number with tail recursion
   def fib(n: Int): Int = {
     @tailrec
     def loop(n: Int, prev: Int, curr: Int): Int = {
-      if(n <= 0) prev
+      if (n <= 0) prev
       else loop(n - 1, curr, curr + prev)
     }
+
     loop(n, 0, 1)
   }
 
+  //Exercise 2.2
   def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     @scala.annotation.tailrec
     def loop(n: Int): Boolean = {
-      if(n >= as.length - 1) true
-      else if(!ordered(as(n), as(n + 1))) false
+      if (n >= as.length - 1) true
+      else if (!ordered(as(n), as(n + 1))) false
       else loop(n + 1)
     }
+
     loop(0)
   }
 
+  //Exercise 2.3
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => f(a, _)
 
+  //Exercise 2.4
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = (a, b) => f(a)(b)
 
+  //Exercise 2.5
   def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
 }
