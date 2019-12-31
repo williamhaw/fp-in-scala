b77dbe9fecd804df4eaf9364b3634a5b42ecfa2a
William Haw <william.haw@agoda.com>
2020-01-01 01:05:59 +0800

    Exercise 6.10 implementation

--- src/main/scala/chapter6/chapter6.scala
+++ src/main/scala/chapter6/chapter6.scala
@@ -103,5 +103,33 @@ package object chapter6 {
     flatMap(s) { a => unit(f(a)) }
 
   def map2WithFlatMap[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    flatMap(ra){ a => map(rb)(b => f(a, b))}
+    flatMap(ra) { a => map(rb)(b => f(a, b)) }
+
+
+  //Exercise 6.10
+  case class State[S, +A](run: S => (A, S)) {
+
+    import State._
+
+    def flatMap[B](f: A => State[S, B]): State[S, B] = State(
+      s => {
+        val (a, nextS) = run(s)
+        f(a).run(nextS)
+      }
+    )
+
+    def map[B](f: A => B): State[S, B] =
+      flatMap(a => unit(f(a)))
+
+    def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+      flatMap(a => sb.map(b => f(a, b)))
+  }
+
+  object State {
+    def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+    def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] =
+      fs.foldRight(unit[S, List[A]](List[A]()))((head, tail) => head.map2(tail) { case (h, t) => h :: t })
+  }
+
 }
