4ae62e06731df75f6f4798a62d4684562ece9766
William Haw <william.haw@agoda.com>
2020-01-01 13:45:44 +0800

    Exercise 6.10 tests for map2, flatMap and sequence

--- src/test/scala/chapter6/Chapter6Spec.scala
+++ src/test/scala/chapter6/Chapter6Spec.scala
@@ -86,22 +86,72 @@ class Chapter6Spec extends FunSuite with Matchers {
   }
 
   sealed trait DoorState
+
   case object Open extends DoorState
+
   case object Closed extends DoorState
 
   case class Door(state: DoorState)
 
   test("State unit") {
-    State.unit[Door, DoorState](Open).run(Door(Closed)) shouldEqual (Open, Door(Closed))
+    State.unit[Door, DoorState](Open).run(Door(Closed)) shouldEqual(Open, Door(Closed))
   }
 
   test("State map") {
     //if door is open, close it and return closed door
     //else open the door and return open door
-    val initialState = State[Door, DoorState](d => if(d.state == Open) (Closed, Door(Closed)) else (Open, Door(Open)))
+    val initialState = State[Door, DoorState](d => if (d.state == Open) (Closed, Door(Closed)) else (Open, Door(Open)))
     //door open initially, after transition will be closed and mapped to 0
-    initialState.map(ds => if(ds == Open) 1 else 0).run(Door(Open)) shouldEqual (0, Door(Closed))
+    initialState.map(ds => if (ds == Open) 1 else 0).run(Door(Open)) shouldEqual(0, Door(Closed))
     //door closed initially, after transition will be open and mapped to 1
-    initialState.map(ds => if(ds == Open) 1 else 0).run(Door(Closed)) shouldEqual (1, Door(Open))
+    initialState.map(ds => if (ds == Open) 1 else 0).run(Door(Closed)) shouldEqual(1, Door(Open))
+  }
+
+  test("State map2") {
+    //if door is open, close it and return closed door
+    //else open the door and return open door
+    val initialState = State[Door, DoorState](d => if (d.state == Open) (Closed, Door(Closed)) else (Open, Door(Open)))
+    //leave door as is
+    val initialState2 = State[Door, DoorState](d => if (d.state == Open) (Open, Door(Open)) else (Closed, Door(Closed)))
+
+    def mapLogic(a: DoorState, b: DoorState): Int = (a, b) match {
+      case (Closed, Closed) => 1
+      case (Closed, Open) => 2
+      case (Open, Closed) => 3
+      case (Open, Open) => 4
+    }
+
+    initialState.map2(initialState2)(mapLogic).run(Door(Open)) shouldEqual(1, Door(Closed))
+    initialState2.map2(initialState)(mapLogic).run(Door(Closed)) shouldEqual(2, Door(Open))
+    initialState2.map2(initialState)(mapLogic).run(Door(Open)) shouldEqual(3, Door(Closed))
+    initialState.map2(initialState2)(mapLogic).run(Door(Closed)) shouldEqual(4, Door(Open))
+  }
+
+  sealed trait LightState
+
+  case object On extends LightState
+
+  case object Off extends LightState
+
+  test("State flatMap") {
+    //if door is open, close it and return closed door
+    //else open the door and return open door
+    val initialState = State[Door, DoorState](d => if (d.state == Open) (Closed, Door(Closed)) else (Open, Door(Open)))
+
+    def doorToLight(doorState: DoorState) = State[Door, LightState](d => if (doorState == Open) (On, d) else (Off, d))
+
+    initialState.flatMap(doorToLight).run(Door(Open)) shouldEqual(Off, Door(Closed))
+    initialState.flatMap(doorToLight).run(Door(Closed)) shouldEqual(On, Door(Open))
+  }
+
+  test("State sequence") {
+    //if door is open, close it and return closed door
+    //else open the door and return open door
+    val initialState = State[Door, DoorState](d => if (d.state == Open) (Closed, Door(Closed)) else (Open, Door(Open)))
+    //leave door as is
+    val initialState2 = State[Door, DoorState](d => if (d.state == Open) (Open, Door(Open)) else (Closed, Door(Closed)))
+
+    State.sequence(List(initialState, initialState2)).run(Door(Open)) shouldEqual(List(Closed, Closed), Door(Closed))
+    State.sequence(List(initialState, initialState2)).run(Door(Closed)) shouldEqual(List(Open, Open), Door(Open))
   }
 }
