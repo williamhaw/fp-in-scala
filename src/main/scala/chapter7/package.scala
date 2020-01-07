35a9609b16bc054e298ad46fa26702eeb04cb17d
William Haw <william.haw@agoda.com>
2020-01-07 18:25:09 +0800

    Exercise 7.1

---
+++ src/main/scala/chapter7/package.scala
@@ -0,0 +1,40 @@
+import java.util.concurrent.{Callable, ExecutorService, Future}
+
+import scala.concurrent.duration.TimeUnit
+
+package object chapter7 {
+
+  object Par {
+
+    type Par[A] = ExecutorService => Future[A]
+
+    //Exercise 7.1
+    def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
+
+    def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
+
+    def fork[A](a: Par[A]): Par[A] = es => es.submit(new Callable[A] {
+      def call = a(es).get
+    })
+
+    def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+//    def run[A](a: Par[A]): A = a
+
+    private case class UnitFuture[A](get: A) extends Future[A] {
+      def isDone = true
+
+      def get(timeout: Long, units: TimeUnit) = get
+
+      def isCancelled = false
+
+      def cancel(evenIfRunning: Boolean): Boolean = false
+    }
+
+  }
+
+}
